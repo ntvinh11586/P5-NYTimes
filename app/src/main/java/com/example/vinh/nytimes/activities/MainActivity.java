d91595299b82d6c304b545872286de2bdba027ad
ntvinh11586 <ntvinh.11586@gmail.com>
2016-08-29 16:01:55 +0700

    Change custom ListView to custom RecycledView

--- app/src/main/java/com/example/vinh/nytimes/activities/MainActivity.java
+++ app/src/main/java/com/example/vinh/nytimes/activities/MainActivity.java
@@ -5,20 +5,19 @@ import android.os.Bundle;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.GridView;
 import android.widget.Toast;
 
 import com.example.vinh.nytimes.Article;
-import com.example.vinh.nytimes.ArticleArrayAdapter;
+import com.example.vinh.nytimes.ContactsAdapter;
+import com.example.vinh.nytimes.ItemClickSupport;
 import com.example.vinh.nytimes.R;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.JsonHttpResponseHandler;
@@ -34,14 +33,15 @@ import cz.msebera.android.httpclient.Header;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText etQuery;
-    GridView gvResults;
-    Button btnSearch;
+//    EditText etQuery;
+//    GridView gvResults;
+//    Button btnSearch;
+    RecyclerView rvResult;
 
     ArrayList<Article> articles;
-    ArticleArrayAdapter adapter;
+//    ArticleArrayAdapter adapter;
     String searchQuery = "";
-
+    ContactsAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -89,23 +89,50 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void setupViews() {
-        gvResults = (GridView)findViewById(R.id.gvResults);
+        rvResult = (RecyclerView)findViewById(R.id.rvContacts);
+
+//        gvResults = (GridView)findViewById(R.id.gvResults);
         articles = new ArrayList<>();
-        adapter = new ArticleArrayAdapter(this, articles);
-        gvResults.setAdapter(adapter);
+//        adapter = new ArticleArrayAdapter(this, articles);
 
-        gvResults.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int pos, long l) {
-                Intent i = new Intent(getApplicationContext(), ArticleActivity.class);
+        adapter = new ContactsAdapter(this, articles);
+        rvResult.setAdapter(adapter);
+//        rvResult.setLayoutManager(new LinearLayoutManager(this));
 
-                Article article = articles.get(pos);
+        // First param is number of columns and second param is orientation i.e Vertical or Horizontal
+        StaggeredGridLayoutManager gridLayoutManager =
+                new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
+        // Attach the layout manager to the recycler view
+        rvResult.setLayoutManager(gridLayoutManager);
 
-                i.putExtra("article", article);
+//        gvResults.setAdapter(adapter);
 
-                startActivity(i);
-            }
-        });
+//        gvResults.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int pos, long l) {
+//                Intent i = new Intent(getApplicationContext(), ArticleActivity.class);
+//
+//                Article article = articles.get(pos);
+//
+//                i.putExtra("article", article);
+//
+//                startActivity(i);
+//            }
+//        });
+        ItemClickSupport.addTo(rvResult).setOnItemClickListener(
+                new ItemClickSupport.OnItemClickListener() {
+                    @Override
+                    public void onItemClicked(RecyclerView recyclerView, int position, View v) {
+                        Intent i = new Intent(getApplicationContext(), ArticleActivity.class);
+
+                        Article article = articles.get(position);
+
+                        i.putExtra("article", article);
+
+                        startActivity(i);
+                    }
+                }
+        );
     }
 
     public void onArticleSearch() {
@@ -130,10 +157,14 @@ public class MainActivity extends AppCompatActivity {
                 JSONArray articleJsonResults = null;
 
                 try {
-                    adapter.clear();
+//                    adapter.clear();
+                    articles.clear();
+                    adapter.notifyDataSetChanged();
                     articleJsonResults = response.getJSONObject("response").getJSONArray("docs");
-                    adapter.addAll(Article.fromJSONArray(articleJsonResults));
+//                    adapter.addAll(Article.fromJSONArray(articleJsonResults));
 //                    adapter.notifyDataSetChanged();
+                    articles.addAll(Article.fromJSONArray(articleJsonResults));
+                    adapter.notifyDataSetChanged();
                     Log.d("DEBUG", articles.toString());
                 } catch (JSONException e) {
                     e.printStackTrace();
