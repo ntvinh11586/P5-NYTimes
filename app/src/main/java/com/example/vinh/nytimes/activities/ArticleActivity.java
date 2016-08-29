73d74f6a3832ec2e5bc2b6b970281c23d269cb78
ntvinh11586 <ntvinh.11586@gmail.com>
2016-08-29 16:43:59 +0700

    Add sharing a link in each articles

--- app/src/main/java/com/example/vinh/nytimes/activities/ArticleActivity.java
+++ app/src/main/java/com/example/vinh/nytimes/activities/ArticleActivity.java
@@ -1,7 +1,13 @@
 package com.example.vinh.nytimes.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -29,7 +35,23 @@ public class ArticleActivity extends AppCompatActivity {
         });
 
         webView.loadUrl(article.getWebUrl());
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_article, menu);
 
+        MenuItem item = menu.findItem(R.id.menu_item_share);
+        ShareActionProvider miShare = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+
+        WebView wvArticle = (WebView) findViewById(R.id.wvArticle);
+        shareIntent.putExtra(Intent.EXTRA_TEXT, wvArticle.getUrl());
+
+        miShare.setShareIntent(shareIntent);
+
+        return super.onCreateOptionsMenu(menu);
     }
 }
